efdcfd3261e8906b3ef9304ed21c5e6908dbdaa1
elissa <github@blahai.gay>
2024-05-01 18:10:01 +0300

    Update update-ags.sh

--- update-ags.sh
+++ update-ags.sh
@@ -4,7 +4,7 @@ cd ./cache/ags
 git init -b main
 git remote add origin https://github.com/Aylur/ags.git
 git pull origin main && git submodule --init --recursive
-git checkout f905abfd2210701929ffaa3db13faa783b782765 # "update examples"
+git checkout c93e32cc5f822e99303b9d452f8a845ef207bb66
 npm install
 meson setup build
 meson install -C build
