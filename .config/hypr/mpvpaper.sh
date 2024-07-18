9fe546189a1ab88259472abf3c82af5fc1e415ed
elissa <github@blahai.gay>
2024-07-18 09:50:12 +0300

    idek

--- .config/hypr/mpvpaper.sh
+++ .config/hypr/mpvpaper.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-if [[ -f "~/Pictures/wallpapers/videos/current" ]]; then
-  mpvpaper DP-1 -f -o "loop panscan=1.0" ~/Pictures/wallpapers/videos/current
+if [ -L "~/Pictures/wallpapers/videos/current" ]; then
+  echo "silly"
+  # mpvpaper DP-1 -f -o "loop panscan=1.0" ~/Pictures/wallpapers/videos/current
 fi
